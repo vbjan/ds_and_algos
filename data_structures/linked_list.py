cce0e030fe1748a1a27eb024b75faee56e78ed0b
vbjan <62449932+vbjan@users.noreply.github.com>
2024-02-26 15:21:57 +0100

    Including feedback to implementation of linked list.

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -4,7 +4,7 @@ from typing import Optional
 class Node:
     def __init__(self, element: int):
         self.element: int = element
-        self.next: Optional[Node, None] = None
+        self.next: Optional[Node] = None
 
     def __repr__(self):
         return f"Node({self.element})->"
@@ -12,9 +12,9 @@ class Node:
 
 class LinkedList:
     def __init__(self):
-        self.head: Optional[Node, None] = None
+        self.head: Optional[Node] = None
         self.size: int = 0
-        self.tail: Optional[Node, None] = None
+        self.tail: Optional[Node] = None
 
     def append(self, new_node: Node) -> None:  # O(1)
         if self.head is None:
@@ -40,7 +40,7 @@ class LinkedList:
         return self.size == 0
 
     def index_is_allowed(self, i):
-        return 0 <= i <= self.size - 1
+        return 0 <= i <= self.size
 
     def insert(self, new_node: Node, i: int) -> None:
         if not self.index_is_allowed(i):
@@ -50,6 +50,8 @@ class LinkedList:
             new_node.next = self.head
             self.head = new_node
             self.size += 1
+        elif i == self.size:  # inserting at last element = appending, O(1)
+            self.append(new_node)
         else:  # O(n) because of traversing through list
             prev_node = self[i-1]
             temp = prev_node.next
@@ -58,16 +60,12 @@ class LinkedList:
             self.size += 1
 
     def __repr__(self) -> str:  # O(n)
-        node, name = self.head, ""
+        node, node_names = self.head, []
         while node:
-            if node is self.head:
-                name += f"Head{str(node)}"
-            elif node is self.tail:
-                name += f"Tail{str(node)}"
-            else:
-                name += str(node)
+            node_name = f"Head{str(node)}" if node is self.head else f"Tail{str(node)}" if node is self.tail else str(node)
+            node_names.append(node_name)
             node = node.next
-        return f"LinkedList[size={self.size}]: {name}None"
+        return f"LinkedList[size={self.size}]: {''.join(node_names)}None"
 
 
 if __name__ == "__main__":
@@ -78,7 +76,7 @@ if __name__ == "__main__":
     print(list)
     print(list[1])
     print(list[2])
-    list.insert(Node(10), 2)
+    list.insert(Node(10), 3)
     print(list)
     list.insert(Node(11), 0)
     print(list)
