1cd2703101668a9515ef7ba351d12f146e9c2c71
vbjan <62449932+vbjan@users.noreply.github.com>
2024-02-28 15:28:12 +0100

    Intersect and union method added to HashSet

--- hash_set/hash_set.py
+++ hash_set/hash_set.py
@@ -1,4 +1,5 @@
 from linked_list.linked_list import LinkedList
+from copy import deepcopy
 
 
 class HashSet:
@@ -10,7 +11,8 @@ class HashSet:
         self.store = [LinkedList() for _ in range(self.max_num_elements)]
 
     def add(self, element) -> None:
-        local_linked_list = self.store[self.storage_index(element)]
+        idx = self.storage_index(element)
+        local_linked_list = self.store[idx]
         if element in local_linked_list:  # element already present in HashSet
             return
 
@@ -58,6 +60,29 @@ class HashSet:
 
         self.update_load_factor()
 
+    def intersect(self, other: 'HashSet') -> 'HashSet':
+        smaller_set = other if len(other) < len(self) else self
+        larger_set = self if len(other) < len(self) else other
+        result_set = HashSet(len(smaller_set))
+
+        for linked_list in smaller_set.store:
+            for element in linked_list:
+                if element in larger_set:
+                    result_set.add(element)
+
+        return result_set
+
+    def union(self, other: 'HashSet') -> 'HashSet':
+        smaller_set = other if len(other) < len(self) else self
+        larger_set = self if len(other) < len(self) else other
+        result_set = deepcopy(larger_set)
+
+        for linked_list in smaller_set.store:
+            for element in linked_list:
+                result_set.add(element)
+
+        return result_set
+
 
 if __name__ == "__main__":
     test_set = HashSet(max_num_elements=2)
@@ -73,6 +98,12 @@ if __name__ == "__main__":
     print(None in test_set)
     test_set.remove("two")
     print(test_set)
+
+    test_set_2 = HashSet(max_num_elements=2)
+    test_set_2.add("one")
+    print(test_set_2)
+
+    print(test_set_2.union(test_set))
 
 
 
